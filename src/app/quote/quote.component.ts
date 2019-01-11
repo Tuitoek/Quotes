f484a847a12d49d199f6de663eb58325e35e6a44
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-11 10:21:53 +0300

    Add goal component and goal class

---
+++ src/app/quote/quote.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote'
+
+@Component({
+  selector: 'app-quote',
+  templateUrl: './quote.component.html',
+  styleUrls: ['./quote.component.css']
+})
+export class QuoteComponent implements OnInit {
+  quotes=[
+    new Quote(1,"We have to do the best we are capable of.This is our sacred human responsibility.~Albert Einsten."),
+    new Quote(2,"Two roads diverged in a wood,and I took the one less travelled by,and that has made all the difference.~Robert Frost.")
+  ]
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
