3b0dfc4f99b7864337c7b5204a28f459c60db786
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-12 11:49:03 +0300

    Add all changes

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,6 +1,5 @@
 import { Component, OnInit } from '@angular/core';
 import {Quote} from '../quote'
-import{Form} from '../quotes-form'
 
 @Component({
   selector: 'app-quote',
@@ -12,7 +11,7 @@ export class QuoteComponent implements OnInit {
     new Quote(1,"We have to do the best we are capable of.This is our sacred human responsibility.~Albert Einsten."),
     new Quote(2,"Two roads diverged in a wood,and I took the one less travelled by,and that has made all the difference.~Robert Frost.")
   ]
-  
+
   constructor() { }
 
   ngOnInit() {
