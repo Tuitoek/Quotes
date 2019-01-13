e8802fdf830953e20c3468280670b8af24ab41a3
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-13 06:46:04 +0300

    Add an author and time

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote-details',
@@ -6,7 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-
+  quotes=[
+    new Quote(1,"We have to do the best we are capable of.This is our sacred human responsibility.","~Albert Einsten.",2018/8/12),
+    new Quote(2,"Two roads diverged in a wood,and I took the one less travelled by,and that has made all the difference.","~Robert Frost.",2018/9/3),
+  ]
   constructor() { }
 
   ngOnInit() {
--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -8,8 +8,8 @@ import {Quote} from '../quote'
 })
 export class QuoteComponent implements OnInit {
   quotes=[
-    new Quote(1,"We have to do the best we are capable of.This is our sacred human responsibility."),
-    new Quote(2,"Two roads diverged in a wood,and I took the one less travelled by,and that has made all the difference.")
+    new Quote(1,"We have to do the best we are capable of.This is our sacred human responsibility.","~Albert Einsten.",2018/8/12),
+    new Quote(2,"Two roads diverged in a wood,and I took the one less travelled by,and that has made all the difference.","~Robert Frost.",2018/9/3),
   ]
 
   constructor() { }
