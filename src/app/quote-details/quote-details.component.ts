e984526eb2e332ea900eebe63d46a7fa64081466
Sarah Tuitoek <sjtoek@gmail.com>
2019-01-14 09:36:14 +0300

    Updated author and date

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input} from '@angular/core';
+import { Component, OnInit} from '@angular/core';
 import {Quote} from '../quote';
 
 @Component({
@@ -8,10 +8,9 @@ import {Quote} from '../quote';
 })
 export class QuoteDetailsComponent implements OnInit {
   quotes=[
-    new Quote(1,"We have to do the best we are capable of.This is our sacred human responsibility","~Albert Einsten.",2018/8/12),
-    new Quote(2,"Two roads diverged in a wood,and I took the one less travelled by,and that has made all the difference","~Robert Frost.",2018/9/3),
+    new Quote(1,"We have to do the best we are capable of.This is our sacred human responsibility","~Albert Einsten.",new Date(2018,8,12)),
+    new Quote(2,"Two roads diverged in a wood,and I took the one less travelled by,and that has made all the difference","~Robert Frost.",new Date (2018,9,16)),
   ]
-    @Input() quote:Quote;
 
   constructor() { }
 
--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,4 +1,4 @@
 export class Quote {
   public hide:boolean = true;
-  constructor(public id:number,public name:string,public description:string,public date:number){}
+  constructor(public id:number,public name:string,public description:string,public date:Date){}
 }
